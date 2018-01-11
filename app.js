4b4eb8f3e4939235208b3c4bf85265d82af99afe
vagrant <vagrant@vhost1.vagrant.dev>
2018-01-11 16:20:24 +0900

    delete unnecessary comment

--- app.js
+++ app.js
@@ -57,15 +57,15 @@ function get_tweetID(diff){
         }
         docs.forEach(function(doc) {
             console.log(doc.id_str);
-            io.sockets.emit('emit_from_server_tweetID', doc.id_str);
+            io.sockets.emit('emit_from_server_tweetID', doc.id_str);    // クライアントにツイートIDを送信
         });
     });
 }
 
-function db_reConnect(){
-    db.close();
-    db = mongojs(db_url);
-}
+// function db_reConnect(){
+//     db.close();
+//     db = mongojs(db_url);
+// }
 
 io.sockets.on('connection', function(socket){
     socket.on('emit_from_client', function(data){
@@ -74,7 +74,6 @@ io.sockets.on('connection', function(socket){
           if (err) { console.log(err); }
           console.log(stdout);
         });
-        // socket.emit('emit_from_server', 'client says :'+data);
     });
 });
 
@@ -92,79 +91,5 @@ setImmediate(function(){
 
 setInterval(check_mongoDB, 500);    // mongoDBへのツイート追加をチェック
 setInterval(function(){
-    console.log(global.prev_count);
+    console.log(global.prev_count); // デバッグ用
 },500);
-
-
-
-// setInterval(function(){
-//     // console.log("prev_count typeof :" + typeof prev_count);
-//     // console.log("(out)prev_count :" + global.prev_count);
-//     console.log("count typeof :" + typeof count);
-//     console.log("(out)count :" + count);
-// },1000);
-
-/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-// http.createServer(function (req, res) {
-//   console.log("listen on 8000...")
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello Hosomichi!\n');
-// }).listen(8080, '127.0.0.1');
-// console.log("listen on 8000...")
-
-// var command = 'python3.6 get_tweet.py 広島 10';
-
-// const exec = require('child_process').exec;
-// exec(command, (err, stdout, stderr) => {
-//   if (err) { console.log(err); }
-//   console.log(stdout);
-// });
-
-// app.get('/', function(request, response){ 
-//       fs.readFile('./index.html', 'UTF-8', 
-//             function (err, data) { 
-//                 response.writeHead(200, {'Content-Type': 'text/html'}); 
-//                 response.write(data); 
-//                 response.end(); 
-//             } 
-//         );
-
-// })
-
-// app.get('/app.js', function(req, res) {
-//     res.send('<h1>Hello World!</h1>');
-// });
-
-// app.listen(8000, function() {
-//     console.log('Express Listen port 8000');
-// })
-
-
-/* 3. 以後、アプリケーション固有の処理 */
-
-// var mongojs = require('mongojs');
-// var db = mongojs('mongodb://127.0.0.1:27017/eventtweet');
-// var tweetdata = db.collection('tweetdata')
-// すべての Document を削除
-// db.mycollection.remove();
-
-// Document 追加
-// db.testcol.insert({name: 'maku'});
-// db.testcol.insert({name: 'moja'});
-// db.testcol.insert({name: 'mayy'});
-
-// Document を検索
-// db.tweetdata.find(function(err, docs) {
-//     if (err) {
-//         console.log('Error!');
-//         return;
-//     }
-
-//     docs.forEach(function(doc) {
-//         console.log(doc.text);
-//         // console.log(doc);
-//         console.log("forEach!!!!!");
-//     });
-
-//     db.close();
-// });
